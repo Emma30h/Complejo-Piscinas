c2ee025b78b48df68edefb911c88f4813edcbf7f
Emma30h <emma30h@gmail.com>
2022-06-14 19:26:51 -0300

    Actualizacion II

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ setInterval(function(){
         counter = 1;
     }
 }, 5000);
+
+//GALERIA
+const images = document.querySelectorAll(".img");
+const containerLightbox = document.querySelector(".container-lightbox");
+const imageContainer = document.querySelector(".img-show");
+const copy = document.querySelector(".copy");
+const closeModal = document.querySelector(".bx.bx-x");
+
+images.forEach(image=>{
+    image.addEventListener("click", ()=>{
+        addImage(image.getAttribute("src"), image.getAttribute("alt"));
+    });
+});
+
+const addImage = (srcImage, altImage)=>{
+    containerLightbox.classList.toggle("move");
+    imageContainer.classList.toggle("show");
+    imageContainer.src = srcImage;
+    copy.innerHTML = altImage;
+}; 
+
+closeModal.addEventListener("click", ()=>{
+    containerLightbox.classList.toggle("move");
+    imageContainer.classList.toggle("show");
+});
